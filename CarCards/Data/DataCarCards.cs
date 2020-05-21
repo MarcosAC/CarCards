55d9391cde7cc3989cd62581d6ea5a303ff304fa
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-05-21 11:36:06 -0300

    Comando Adicionar

--- CarCards/Data/DataCarCards.cs
+++ CarCards/Data/DataCarCards.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace CarCards.Data
---
+++ CarCards/ViewModels/AddCardPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using CarCards.Data;
+using CarCards.Models;
+using Prism.Commands;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarCards.ViewModels
+{
+    public class AddCardPageViewModel : BindableBase
+    {
+        readonly DataCarCards DataCarCards = new DataCarCards();        
+
+        readonly Card card = new Card
+        {
+
+        };
+
+        private DelegateCommand _addCard;
+
+        public DelegateCommand AddCard => _addCard ?? (new DelegateCommand(() => ExecuteAddCard(card)));
+
+        private void ExecuteAddCard(Card card)
+        {
+            DataCarCards.Add(card);
+        }
+    }
+}
