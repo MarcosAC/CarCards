6dbdfae1a2737db2c04150c14a0221851e9334d0
Marcos Aurelio <marcos.aucorrea@gmail.com>
2020-05-23 20:33:26 -0300

    Adicionar novo cartão

--- CarCards/ViewModels/AddCardPageViewModel.cs
+++ CarCards/ViewModels/AddCardPageViewModel.cs
@@ -2,10 +2,6 @@
 using CarCards.Models;
 using Prism.Commands;
 using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CarCards.ViewModels
 {
